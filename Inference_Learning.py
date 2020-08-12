fe1aac49366c68e7a95bbcd748a454c88e1364eb
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2020-08-12 11:25:48 +0200

    Add files via upload
    first commit

---
+++ Inference_Learning.py
@@ -0,0 +1,24 @@
+#     _   __                      ______
+#    / | / /__  __  ___________  / ____/___  ________
+#   /  |/ / _ \/ / / / ___/ __ \/ /   / __ \/ ___/ _ \
+#  / /|  /  __/ /_/ / /  / /_/ / /___/ /_/ / /  /  __/
+# /_/ |_/\___/\__,_/_/   \____/\____/\____/_/   \___/
+
+# NeuroCore is an PyTorch implementation of a Predictive Visual Network for robotics
+# applications (tracking, recognition, manipulation,...)
+
+# Author : Munch Quentin, 2020
+
+# General library
+import numpy as np
+import cv2
+import time
+from matplotlib import pyplot as plt
+from matplotlib import pyplot
+
+# Pytorch library
+import torch
+import torch.nn as nn
+from torch.nn import init
+# custom layer
+from NeuroCore_Model import *
