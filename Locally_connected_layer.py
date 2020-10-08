6d89daf1354ef69d083f5791dc8e0eff78a2f49b
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2020-10-08 17:34:01 +0200

    Update Locally_connected_layer.py
    functional function for weight matrix generation

--- Locally_connected_layer.py
+++ Locally_connected_layer.py
@@ -4,10 +4,12 @@ import itertools
 
 def compute_output_shape(inputShape, kernelShape, strideShape, paddingMode):
     if paddingMode == True:
-        return  [((inputShape[index]+strideShape[index]-1)//strideShape[index]) for index in range(len(inputShape))]
+        outShape = [((inputShape[index]+strideShape[index]-1)//strideShape[index]) for index in range(len(inputShape))]
+        outShape = tuple([0 if inputShape[index] == 0 else outShape[index] for index in range(len(inputShape))])
     else:
-        return [((inputShape[index]-kernelShape[index]+1+strideShape[index]-1)//strideShape[index]) for index in range(len(inputShape))]
-
+        outShape = [((inputShape[index]-kernelShape[index]+1+strideShape[index]-1)//strideShape[index]) for index in range(len(inputShape))]
+        outShape = tuple([0 if inputShape[index] == 0 else outShape[index] for index in range(len(inputShape))])
+    return outShape
 
 # Return locations of the input connected to an output position.
 def location_output_input(inputShape, kernelShape, strideShape, outPosition, paddingMode):
@@ -32,14 +34,10 @@ def location_output_input(inputShape, kernelShape, strideShape, outPosition, pad
     # return position
     return location
 
-
 # Yields output-input tuples of indices in a layer.
 def kernel_index(inputShape, kernelShape, strideShape, paddingMode, channelIn, channelOut):
     # input dimension
     inDim = len(inputShape)
-    # get kernel/stride size over the input dimension
-    kernelShape = int((kernelShape,))*inDim
-    strideShape = int((strideShape,))*inDim
     # kernel and stride dimension
     kernelDim = len(kernelShape)
     strideDim = len(strideShape)
@@ -56,5 +54,5 @@ def kernel_index(inputShape, kernelShape, strideShape, paddingMode, channelIn, c
             for fanIn in range(channelIn):
                 for fanOut in range(channelOut):
                     outIdx = np.ravel_multi_index(multi_index=concatIdxs(outputPosition, fanOut), dims=concatIdxs(outputShape, channelOut))
-                    inIdx = np.ravel_multi_index(multi_index=concat_idxs(inputPosition, fanIn), dims=concat_idxs(inputShape, channelIn))
+                    inIdx = np.ravel_multi_index(multi_index=concatIdxs(inputPosition, fanIn), dims=concatIdxs(inputShape, channelIn))
                     yield (outIdx, inIdx)
